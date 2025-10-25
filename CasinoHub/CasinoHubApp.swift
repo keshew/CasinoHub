13805ef52e34296f2b35bb3bde30726df51b0df5
keshew <artyom.korotckov2017@yandex.ru>
2025-10-25 12:51:14 +0500

    Initial Commit

---
+++ CasinoHub/CasinoHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  CasinoHubApp.swift
+//  CasinoHub
+//
+//  Created by Артём Коротков on 25.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct CasinoHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
